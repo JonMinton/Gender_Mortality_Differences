442bb97d40ea38cc805da67483eb312e838f8baf
Jonathan Minton <nate.minton@gmail.com>
2014-08-12 09:06:05 +0100

    first commit of gender mortality code

---
+++ script.r
@@ -0,0 +1,13 @@
+# 12 /8 / 2014
+
+# Main script for gender mortality differences paper
+
+
+# Actions:
+# 1) include usual package pre-amble
+# 2) load 'tidy data' from population age residuals paper
+# 3) Create mortality bathtub curve showing differences in mortality from males to females
+# 4) reproduce existing figures included
+
+
+
